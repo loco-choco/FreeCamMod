04639f6a41ade10e6c44177ee2d084842c4360e4
ShoosGun <ivanrwpf@gmail.com>
2020-12-08 13:02:23 -0300

    Free Cam Mod Update yeeey

--- FreeCamMod/FreeCamMod.cs
+++ FreeCamMod/FreeCamMod.cs
@@ -1,14 +1,13 @@
 ﻿using UnityEngine;
-using IMOWAAnotations;
+using CAMOWA;
 
 namespace FCM
 {
 
     public class FreeCamMod : MonoBehaviour
     {
-        
+        public bool isThereAPlayer = false;
 
-        private GameObject freeCamObject;
         private float freeCamRotationX = 0f;
         private float freeCamRotationAround = 0f;
         private Vector3 freeCamPosition;
@@ -23,110 +22,101 @@ namespace FCM
 
         private float timeWhenFreezed = 0f;
 
-
-        [IMOWAModInnit("Marshmallow", "Start", modName = "EnchacedMallows")]
-        public static void  ModInnit(string porOndeTaInicializando)
+        [IMOWAModInnit("Free Cam Mod", -1, 2)]
+        public static void ModInnit(string porOndeTaInicializando)
         {
-            
-            Debug.Log("FreeCamMod foi iniciado em "+ porOndeTaInicializando);
-            GameObject.FindGameObjectsWithTag("MainCamera")[0].AddComponent<FreeCamMod>();
-            Debug.Log("O script do mod foi colocado na 'MainCamera' ");
-
+            Debug.Log("FreeCamMod foi iniciado em " + porOndeTaInicializando);
+            new GameObject("FreeCamModCamera").AddComponent<FreeCamMod>();
+            Debug.Log("FreeCamModCamera GO foi criado");
         }
-
         void Start()
         {
-           
-            currentCameraTransform = gameObject.FindWithRequiredTag("MainCamera").transform;
-
+            Debug.Log("No start, bora-lá");
+            var possiblePlayerArray = (PlayerBody[])FindObjectsOfType(typeof(PlayerBody));
+            if (possiblePlayerArray.Length > 0)
+            {
+                playerTransform = possiblePlayerArray[0].gameObject.transform;
+                playerCamController = playerTransform.GetRequiredComponentInChildren<PlayerLockOnTargeting>();
+                currentCameraTransform = gameObject.FindWithRequiredTag("MainCamera").transform;
+                isThereAPlayer = true;
+            }
+            else
+                currentCameraTransform = ((Camera)FindObjectOfType(typeof(Camera))).transform;
             
-            playerTransform = gameObject.FindWithRequiredTag("Player").transform;
-            playerCamController = playerTransform.GetRequiredComponentInChildren<PlayerLockOnTargeting>();
-            
-
-            freeCamObject = new GameObject("FreeCamModCamera");
-            
-            freeCamObject.AddComponent<Camera>();
-            freeCamObject.camera.enabled = false;
-            freeCamObject.camera.CopyFrom(currentCameraTransform.camera);
-
             playerLockingViewOn = new GameObject("FreeCamModViewLock");
+            
+            gameObject.AddComponent<Camera>();
+            gameObject.camera.enabled = false;
+            gameObject.camera.CopyFrom(currentCameraTransform.camera);
+
+            
 			playerLockingViewOn.transform.position = currentCameraTransform.position + currentCameraTransform.forward;
 
             freeCamPosition = Vector3.zero;
-
-            
-
-
-
         }
 
-       
+        float cameraSpeed = 3f;
 
         void Update()
         {
+           
+            //Quando ele carrega outro cenário, e bagulho não restarta o script, isso é um certo problema
+            //Talvez colocar algum int, e quando carregar mudar o valor do int para indicar em qual está, e ai fazer as correções
+            //Ta tarde, boa sorte na prova amanhã ;;)
 
-            
 
             if (Input.GetKeyDown(KeyCode.CapsLock))
             {
                 if (!IsFreeCamEnable())
                 {
-                    
-                    Debug.Log("Congelando o mov. do player");
-                        
-                    playerTransform.gameObject.GetComponent<CharacterMovementModel>().LockMovement();
-                    playerCamController.LockOn(playerLockingViewOn.transform);
-                    
-
+                    if (isThereAPlayer)
+                    {
+                        Debug.Log("Congelando o mov. do player");
+                        playerTransform.gameObject.GetComponent<CharacterMovementModel>().LockMovement();
+                        playerCamController.LockOn(playerLockingViewOn.transform);
+                    }
                     Debug.Log("Coisas magicas");
                     currentCameraTransform.camera.enabled = false;
-                    freeCamObject.camera.enabled = true;
-
-                    
-                    
-
-             
+                    gameObject.camera.enabled = true;
                 }
-
                 else
                 {
-                    Debug.Log("Descongelando o mov. do player");
-                    playerTransform.gameObject.GetComponent<CharacterMovementModel>().UnlockMovement();
-                    playerCamController.BreakLock();
-                    
+                    if (!((SettingsMenu)FindObjectsOfType(typeof(SettingsMenu))[0]).enabled || Application.loadedLevel == 0)
+                        Time.timeScale = 1f;
+
+                    if (isThereAPlayer)
+                    {
+                        Debug.Log("Descongelando o mov. do player");
+                        playerTransform.gameObject.GetComponent<CharacterMovementModel>().UnlockMovement();
+                        playerCamController.BreakLock();
+                    }
                     Debug.Log("Mais coisas magicas");
                     currentCameraTransform.camera.enabled = true;
-                    freeCamObject.camera.enabled = false;
+                    gameObject.camera.enabled = false;
 
+                    gameObject.transform.position = currentCameraTransform.position;
                     freeCamPosition = Vector3.zero;
                     freeCamRotationAround = 0f;
                     freeCamRotationX = 0f;
-
-                    
-                   
                 }
             }
-
-
-            float cameraSpeed = 3f;
-            
-
             if (IsFreeCamEnable())
             {
-                
-
+                if (Input.GetKeyDown(KeyCode.Z))
+                {
+                    if (Time.timeScale != 0) //Se o timeScale já não é zero, fazer ele virar zero
+                    {
+                        Time.timeScale = 0f;
+                    }//Se estamos no menu, não tem que se preocupar em voltar ao normal, mas se estamos no Solar System, vereficar se o settings menu ta aberto
+                    else if (!((SettingsMenu)FindObjectsOfType(typeof(SettingsMenu))[0]).enabled || Application.loadedLevel == 0)
+                        Time.timeScale = 1f;
+                }
                 //Cuidar da aceleração e veloc. da camera
-                
                 if (Input.GetKey(KeyCode.Tab))
                     cameraSpeed = 9f;
-                
                 else
                     cameraSpeed = 3f;
-
-
                 //Direção q a camera vai virar
-
                 if (Time.timeScale != 0)
                 {
                     freeCamRotationAround += PlayerCameraInput.lookX.GetInput() * Time.deltaTime * 500f % 360;
@@ -134,28 +124,20 @@ namespace FCM
                 }
                 else
                 {
-
                     freeCamRotationAround += PlayerCameraInput.lookX.GetInput() * DeltaTimeWhenFreezed() * 500f % 360;
                     freeCamRotationX -= PlayerCameraInput.lookY.GetInput() * DeltaTimeWhenFreezed() * 500f;
                 }
-
-                
-
-
                 freeCamRotationX = Mathf.Clamp(freeCamRotationX, -90f, 90f);
 
                 if (rotateWithTheCamera)
-                    freeCamObject.transform.rotation = currentCameraTransform.rotation;
+                    gameObject.transform.rotation = currentCameraTransform.rotation;
                 else
-                    freeCamObject.transform.eulerAngles = Vector3.zero;
+                    gameObject.transform.eulerAngles = Vector3.zero;
 
-                freeCamObject.transform.rotation = freeCamObject.transform.rotation *  Quaternion.Euler(freeCamRotationAround * playerLockingViewOn.transform.up) * Quaternion.Euler(freeCamRotationX, 0f, 0f);
-
-
-
-
+                gameObject.transform.rotation = gameObject.transform.rotation *  Quaternion.Euler(freeCamRotationAround * playerLockingViewOn.transform.up) * Quaternion.Euler(freeCamRotationX, 0f, 0f);
+                
                 //Direção q a camera vai andar
-                freeCamObject.transform.position = currentCameraTransform.position;
+                gameObject.transform.position = currentCameraTransform.position;
 
                 //ZA WARUDO
                 float variacaoDoTempo;
@@ -165,33 +147,28 @@ namespace FCM
                 else
                     variacaoDoTempo = DeltaTimeWhenFreezed();
 
-
-                    if (Input.GetKey(KeyCode.W))
-                    freeCamPosition += freeCamObject.transform.forward * cameraSpeed * variacaoDoTempo;
+                if (Input.GetKey(KeyCode.W))
+                    freeCamPosition += gameObject.transform.forward * cameraSpeed * variacaoDoTempo;
 
                 if (Input.GetKey(KeyCode.S))
-                    freeCamPosition += -freeCamObject.transform.forward * cameraSpeed * variacaoDoTempo;
+                    freeCamPosition += -gameObject.transform.forward * cameraSpeed * variacaoDoTempo;
 
                 if (Input.GetKey(KeyCode.D))
-                    freeCamPosition += freeCamObject.transform.right * cameraSpeed * variacaoDoTempo;
+                    freeCamPosition += gameObject.transform.right * cameraSpeed * variacaoDoTempo;
 
                 if (Input.GetKey(KeyCode.A))
-                    freeCamPosition += -freeCamObject.transform.right * cameraSpeed * variacaoDoTempo;
+                    freeCamPosition += -gameObject.transform.right * cameraSpeed * variacaoDoTempo;
 
-                freeCamObject.transform.position += freeCamPosition;
+                gameObject.transform.position += freeCamPosition;
                 //Fazer com q isso sempre esteja uma unidade afrente da camera principal
 
                 playerLockingViewOn.transform.position = currentCameraTransform.position + currentCameraTransform.forward;
 
                 //Ver qual foi o tempo desse frame
                 timeWhenFreezed = Time.realtimeSinceStartup;
-
-
             }
 
         }
-
-        
         //ZA WARUDO
         float DeltaTimeWhenFreezed()
         {
@@ -200,7 +177,7 @@ namespace FCM
 
         bool IsFreeCamEnable()
         {
-            return freeCamObject.camera.enabled;
+            return gameObject.camera.enabled;
         }
 
     } 
---
+++ FreeCamMod/LevelLoaderHandler.cs
@@ -0,0 +1,77 @@
+﻿//using CAMOWA;
+//using UnityEngine;
+
+//namespace FCM
+//{
+//    public class LevelLoaderHandler : MonoBehaviour
+//    {
+
+//        static private bool levelLoaderCreated = false;
+//        private int levelIndex = -1;
+//        [IMOWAModInnit("Free Cam Mod", -1, 2)]
+//        public static void ModInnit(string porOndeTaInicializando)
+//        {
+//            if (!levelLoaderCreated)
+//            {
+//                Debug.Log("FreeCamMod foi iniciado em " + porOndeTaInicializando);
+//                new GameObject("FreeCamLevelLoaderHandler").AddComponent<LevelLoaderHandler>(); // Será que vai funcionar?
+//                Debug.Log("O script do mod foi colocado na 'MainCamera' ");
+
+//                levelLoaderCreated = !levelLoaderCreated;
+//            }
+
+//        }
+
+//        void Awake()
+//        {
+//            DontDestroyOnLoad(gameObject);
+//        }
+
+//        //Ideia, no lugar de fazer patch no Assembly do jogo e ter chance de dar ruim, fazer com que isso ocorra nessa classe, como separação de
+//        //prioridade e em qual level o start vai ocorrer
+//        //Se a pessoa quer que o script dela ocorra antes de algum Awake ou sla, ela faz usando Harmony e tals
+
+//        void MainMenuStart()
+//        {
+//            Debug.Log("MainMenuStart");
+//            Debug.Log("High Priority Mod");
+//            Debug.Log("Regular Priority Mod");
+//            Debug.Log("Low Priority Mod");
+//        }
+
+//        void SolarSystemStart()
+//        {
+//            Debug.Log("SolarSystemStart");
+//            Debug.Log("High Priority Mod");
+//            Debug.Log("Regular Priority Mod");
+//            Debug.Log("Low Priority Mod");
+//        }
+//        void AllLevelStart()
+//        {
+//            Debug.Log("AllLevelStart");
+//            Debug.Log("High Priority Mod");
+//            Debug.Log("Regular Priority Mod");
+//            Debug.Log("Low Priority Mod");
+//            new GameObject("FreeCamModCamera").AddComponent<FreeCamMod>();
+//        }
+
+//        void Update()
+//        {
+//            if (levelIndex != Application.loadedLevel)
+//            {
+//                AllLevelStart();
+//                levelIndex = Application.loadedLevel;
+//                if (levelIndex == 0)
+//                    MainMenuStart();
+//                else if (levelIndex == 1)
+//                    SolarSystemStart();
+
+//            }
+
+//        }
+
+
+
+
+//    }
+//}
